34af6f9733fd25fb9559b08c76d4b98cd54f3f73
askilform <askilform@gmail.com>
2026-01-11 18:08:52 +0100

    ehm whattesigma

---
+++ Assets/Askil/Player/Scripts/Jump.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [NonSerialized] public bool isGrounded = false;
+    private Rigidbody rb;
+    private bool wasHittingLastFrame = false;
+
+    public float jumpForce = 5f;
+    public Animator CameraAnims;
+    public List<AudioSource> SFX = new List<AudioSource>();
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CheckGround();
+        HandleJump();
+    }
+
+    void CheckGround()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        isGrounded = Physics.Raycast(ray, 1.1f);
+
+        if (isGrounded && !wasHittingLastFrame)
+        {
+            OnLanding();
+        }
+
+        wasHittingLastFrame = isGrounded;
+    }
+
+    void HandleJump()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnLanding()
+    {
+        CameraAnims.SetTrigger("Landed");
+        SFX[0].Play();
+    }
+}
---
+++ Assets/Askil/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 6f;
+
+    [Header("Mouse Settings")]
+    public float mouseSensitivity = 2f;
+
+    [Header("References")]
+    public Transform cam;
+    public List<AudioSource> SFX = new List<AudioSource>();
+    public Jump JumpScript;
+
+    private Rigidbody rb;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleMouseLook();
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Combine input directions
+        Vector3 direction = transform.right * x + transform.forward * z;
+
+        // Normalize so diagonals aren't faster
+        if (direction.magnitude > 1f)
+            direction.Normalize();
+
+        Vector3 move = direction * moveSpeed;
+
+        rb.linearVelocity = new Vector3(move.x, rb.linearVelocity.y, move.z);
+        SFX[0].mute = move.x == 0f || move.z == 0f || !JumpScript.isGrounded;
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Rotate the body
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Rotate camera (vertical)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -85f, 85f);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+}
