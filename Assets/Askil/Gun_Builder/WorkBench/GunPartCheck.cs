70bc7f039a5cac7366331c5e629fb6fb052db044
askilform <askilform@gmail.com>
2026-01-14 12:39:48 +0100

    Gym update

--- Assets/Askil/Gun_Builder/WorkBench/GunPartCheck.cs
+++ Assets/Askil/Gun_Builder/WorkBench/GunPartCheck.cs
@@ -8,6 +8,7 @@ public class GunPartCheck : MonoBehaviour
     public int CorrentPartAmount;
     public GameObject SpawnFrom;
     public GameObject WeaponToSpawn;
+    public List <AudioSource> SFXs = new List <AudioSource>();
     void OnEnable()
     {
         Weapon_Builder weapon_BuilderSC = GameObject.Find("WeaponBuilder").GetComponent<Weapon_Builder>();
@@ -16,11 +17,18 @@ public class GunPartCheck : MonoBehaviour
         {
             SpawnWeapon();
         }
+
+        else OnBuildFail();
     }
 
     private void SpawnWeapon()
     {
         Instantiate(WeaponToSpawn, SpawnFrom.transform.position, SpawnFrom.transform.rotation);
+        SFXs[1].Play();
     }
 
+    private void OnBuildFail()
+    {
+        SFXs[0].Play();
+    }
 }
