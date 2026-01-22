714b3b32d7f3a7d93b03d07423458b38b3a4b829
MarcyQweer <backstreetcorp@gmail.com>
2026-01-22 16:07:15 +0100

    Lgihts flighter nothing

---
+++ Assets/PillowBiter/Script/FlickeringLight.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light light;
+
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    public Material flickerMaterial;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+    }
+
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
+    }
+}
