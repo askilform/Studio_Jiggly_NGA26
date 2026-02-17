579e2b5e34fb6fb8e69256603633238600eb6e51
ArgMarc <arg.marcz@gmail.com>
2026-02-17 12:38:19 +0100

    warztexsdycrfvtugbhknjl

--- Assets/Shahin/NaughtyBoi/CigThrowScript.cs
+++ Assets/Shahin/NaughtyBoi/CigThrowScript.cs
@@ -7,9 +7,20 @@ public class CigThrowScript : MonoBehaviour
 
     public bool mustBeHeldToUse = false;
 
+    public GameObject[] cigs;
+
+    private int currentCig = 0;
+
+    private float cigHeightActive = 0f;
+    private float cigHeightPassive = 0f;
+
     void Start()
     {
+        print(currentCig.ToString() + "current cig amount");
         
+        cigHeightActive = cigs[currentCig-1].transform.localPosition.y;
+        cigHeightPassive = cigs[0].transform.localPosition.y;
+
     }
 
 
@@ -18,18 +29,32 @@ public class CigThrowScript : MonoBehaviour
         if (!mustBeHeldToUse && Input.GetKeyDown(KeyCode.G))
         {
             ThrowCig();
-        
         }
+
+
+       Vector3 targetPos = new Vector3(cigs[currentCig].transform.localPosition.x, cigHeightActive, cigs[currentCig].transform.localPosition.z);
+       cigs[currentCig].transform.localPosition = Vector3.Lerp(cigs[currentCig].transform.localPosition, targetPos, Time.deltaTime * 10);
+
     }
+
+
 
     public void ThrowCig()
     {
-        GameObject cigInstance = Instantiate(cigaretteObject, transform.position, transform.rotation);
 
-        if (cigInstance.TryGetComponent<Rigidbody>(out Rigidbody rb))
+        if (currentCig < cigs.Length)
         {
-            rb.linearVelocity = cigInstance.transform.forward * 10 + Vector3.up * 2;
-            rb.angularVelocity = new Vector3(0, 5, 0);
+            cigs[currentCig].gameObject.SetActive(false);
+            currentCig += 1;
+
+            GameObject cigInstance = Instantiate(cigaretteObject, transform.position, transform.rotation);
+
+            if (cigInstance.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                rb.linearVelocity = cigInstance.transform.forward * 10 + Vector3.up * 2;
+                rb.angularVelocity = new Vector3(0, 5, 0);
+            }
+
         }
     }
 
