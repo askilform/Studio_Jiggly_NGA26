4fb34266d189b7a59f79b05adafaa6eb2519fd59
ArgMarc <arg.marcz@gmail.com>
2026-08-24 21:03:10 +0200

    ENDLESS CAR RIDE

--- Assets/EndlessRoad.cs
+++ Assets/EndlessRoad.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class EndlessRoad : MonoBehaviour
 {
+
+
+    public GameObject[] roadSegments;
+    public float tileSize = 100f;
+    public float moveSpeed = 40f;
+    public float howFarBack = 200f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        foreach(GameObject part in roadSegments)
+        {
+            part.transform.position -= Vector3.forward * moveSpeed * Time.deltaTime;
+
+            if (part.transform.position.z < howFarBack)
+            {
+                part.transform.position = new Vector3(0, 0, part.transform.position.z + roadSegments.Length * tileSize);
+            }
+        }
     }
 }
