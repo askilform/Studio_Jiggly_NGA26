9578dea848ecde993c1af1d7f81a1427262c7a3c
askilform <askilform@gmail.com>
2026-09-10 13:25:04 +0200

    Revert "aaaa"
    This reverts commit 45977d8f3228693676744bc1ead1047cba6cb602.

--- Assets/Askil/LevelPrefabs/EventOnTrigger/EventOnTrigger.cs
+++ Assets/Askil/LevelPrefabs/EventOnTrigger/EventOnTrigger.cs
@@ -6,18 +6,9 @@ public class EventOnTrigger : MonoBehaviour
     public UnityEvent onTriggerEvent;
     public bool destroyAfterEvent = true;
 
-    public bool InCar;
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Player" && !InCar)
-        {
-            onTriggerEvent.Invoke();
-
-            if (destroyAfterEvent) Destroy(transform.parent.gameObject);
-        }
-
-        if (other.transform.tag == "Car" && InCar)
+        if (other.transform.tag == "Player")
         {
             onTriggerEvent.Invoke();
 
